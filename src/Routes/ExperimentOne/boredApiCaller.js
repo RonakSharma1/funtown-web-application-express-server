1094d843f312840cab8d37d905f8c8d2e9f3cf13
Ronak Sharma <rsharma10@sheffield.ac.uk>
2021-02-21 16:55:30 +0000

    Fix the query flow between UI and Express

--- src/Routes/ExperimentOne/boredApiCaller.js
+++ src/Routes/ExperimentOne/boredApiCaller.js
@@ -2,19 +2,19 @@ const axios = require ('axios')
 
 module.exports = async (req, res) => {
 
-   const {numberOfParticipants} = req.query
+   const {participants} = req.query
    const boredApiUrl = "http://www.boredapi.com/api/activity/";
 
    try {
       const {data} = await axios.get(boredApiUrl, {
          params: {
-             participants: numberOfParticipants
+             participants: participants
          }});
+      console.log(participants)
       res.json(data);
 
    } catch(error){
       console.log(error.message)
       res.sendStatus(500)
    }
-   
 }
