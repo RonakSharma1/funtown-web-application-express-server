9d40276f07752b82d590dbacb1668795ae5d5c94
Ronak Sharma <rsharma10@sheffield.ac.uk>
2021-03-29 21:28:37 +0100

    Apply es lint formatting

--- src/Routes/ExperimentOne/boredApiCaller.js
+++ src/Routes/ExperimentOne/boredApiCaller.js
@@ -1,20 +1,19 @@
-const axios = require ('axios')
+const axios = require("axios");
 
 module.exports = async (req, res) => {
+  const { participants } = req.query;
+  const boredApiUrl = "http://www.boredapi.com/api/activity/";
 
-   const {participants} = req.query
-   const boredApiUrl = "http://www.boredapi.com/api/activity/";
-
-   try {
-      const {data} = await axios.get(boredApiUrl, {
-         params: {
-             participants: participants
-         }});
-      console.log(participants)
-      res.json(data);
-
-   } catch(error){
-      console.log(error.message)
-      res.sendStatus(500)
-   }
-}
+  try {
+    const { data } = await axios.get(boredApiUrl, {
+      params: {
+        participants: participants,
+      },
+    });
+    console.log(participants);
+    res.json(data);
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
+};
