b0212d9f0a734ca75cba98da53cf990ed8a7f329
Ronak Sharma <rsharma10@sheffield.ac.uk>
2021-08-10 21:52:11 +0100

    Add Tmdb api caller

--- src/Routes/ExperimentTwo/TMDBApiCaller.js
+++ src/Routes/ExperimentTwo/TMDBApiCaller.js
@@ -2,13 +2,10 @@ const axios = require("axios");
 var config = require("../../config.js");
 
 module.exports = async (req, res) => {
-  //Solve how to pass the page number as parameter
-
-  // const { pageNumber } = req.query;
-  const tmdbApiKey = config.TMDB.tmdbApiKey;
-  // const tmdbApiUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${tmdbApiKey}&page=${pageNumber}`;
-  const tmdbApiUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${tmdbApiKey}`;
+  const { pageNumber } = req.query;
+  const { tmdbApiKey } = config.TMDB;
   const tmdbGenreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${tmdbApiKey}`;
+  const tmdbApiUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${tmdbApiKey}&page=${pageNumber}`;
 
   try {
     const [tmdbApiData, tmbdGenreData] = await Promise.all([
